af9d2535f6cc8b78c9452a9ae596f9c7c89aa448
bramvandersommen <bram.vandersommen@gmail.com>
2025-12-01 21:55:12 +0100

    Auto-update VIP list - 2025-12-01T15:55:11.858-05:00

--- vip-config.js
+++ vip-config.js
@@ -1,6 +1,55 @@
-// VIP Configuration
+// VIP Configuration - Auto-updated by N8N
+// Last updated: 2025-12-01T20:51:13.068Z
+// Active VIPs: 9
+
 window.LINKEDIN_AI_VIP_CONFIG = {
-  version: "1.0.0",
-  lastUpdated: "2025-12-01T00:00:00Z",
-  vips: []
+  version: "1764622273068", // Unix timestamp as version
+  lastUpdated: "2025-12-01T20:51:13.068Z",
+  vips: [
+  {
+    "name": "Toni Hopponen",
+    "profileUrl": "https://www.linkedin.com/in/tonihopponen/",
+    "profileId": "tonihopponen"
+  },
+  {
+    "name": "Koen Stam",
+    "profileUrl": "https://www.linkedin.com/in/koenstam87/",
+    "profileId": "koenstam87"
+  },
+  {
+    "name": "Kyle R. Bell",
+    "profileUrl": "https://www.linkedin.com/in/bellcopywriting/",
+    "profileId": "bellcopywriting"
+  },
+  {
+    "name": "Kelly Mcguire",
+    "profileUrl": "https://www.linkedin.com/in/kellymcguire22/",
+    "profileId": "kellymcguire22"
+  },
+  {
+    "name": "Bob Mathers",
+    "profileUrl": "https://www.linkedin.com/in/bmathers/",
+    "profileId": "bmathers"
+  },
+  {
+    "name": "TK Kader",
+    "profileUrl": "https://www.linkedin.com/in/tkkader/",
+    "profileId": "tkkader"
+  },
+  {
+    "name": "Defne Gencler",
+    "profileUrl": "https://www.linkedin.com/in/defnegencler/",
+    "profileId": "defnegencler"
+  },
+  {
+    "name": "Tas Bober",
+    "profileUrl": "https://www.linkedin.com/in/tasbober/",
+    "profileId": "tasbober"
+  },
+  {
+    "name": "Alex Stefan",
+    "profileUrl": "https://www.linkedin.com/in/alex-designlion/",
+    "profileId": "alex-designlion"
+  }
+]
 };
