748a7266eaa0a8582d361bc5f14de11fb790590e
bramvandersommen <bram.vandersommen@gmail.com>
2026-04-06 06:00:34 +0200

    Auto-update VIP list - 2026-04-06T00:00:34.127-04:00

--- vip-config.js
+++ vip-config.js
@@ -1,10 +1,10 @@
 // VIP Configuration - Auto-updated by N8N
-// Last updated: 2026-03-30T04:00:35.270Z
+// Last updated: 2026-04-06T04:00:33.675Z
 // Active VIPs: 10
 
 window.LINKEDIN_AI_VIP_CONFIG = {
-  version: "1774843235271", // Unix timestamp as version
-  lastUpdated: "2026-03-30T04:00:35.271Z",
+  version: "1775448033675", // Unix timestamp as version
+  lastUpdated: "2026-04-06T04:00:33.675Z",
   vips: [
   {
     "name": "Toni Hopponen",
